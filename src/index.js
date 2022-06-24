9967b641f2fd71bffd034753bc45d500ce14daf6
rusterz <aaadev32@gmail.com>
2022-06-23 19:27:05 -0700

    header with icon and project logo created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 import './style.css';
+import home from './icons/menu.svg';
 
-let test = document.createElement('div');
-test.id = 'test';
-document.body.appendChild(test);
+const domModule = (() => {
+    return 1;
+
+})();
+
+const elementIdAppend = (element, ID, parent) => {
+    element.id = ID;
+    document.getElementById(parent).appendChild(element);
+}
+
+const elementIdImgAppend = (ID, importedImage, parent) => {
+    let imgElement = document.createElement('img');
+    imgElement.id = ID;
+    imgElement.src = importedImage;
+    document.getElementById(parent).appendChild(imgElement);
+}
+
+const header = () => {
+    let menuIcon = document.createElement('div');
+    elementIdAppend(menuIcon, 'menu-icon', 'header');
+    let projectLogo = document.createElement('div');
+
+    projectLogo.textContent = 'My ToDo App';
+    elementIdAppend(projectLogo, 'project-logo', 'header');
 
 
-console.log('hello');
+    elementIdImgAppend('test-img', home, 'menu-icon');
 
+    console.log('hello');
 
+}
+
+const sidebar = () => {
+
+}
+
+const content = () => {
+
+}
+
+const footer = () => {
+    
+}
+header();
