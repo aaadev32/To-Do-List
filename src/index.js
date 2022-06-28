8976ebedc4906f0a42e738c2780a02ea87cc9ef0
rusterz <aaadev32@gmail.com>
2022-06-27 19:02:43 -0700

    added but did not finish input fields for new task creation function.

--- src/index.js
+++ src/index.js
@@ -1,45 +1,102 @@
 import './style.css';
 import home from './icons/menu.svg';
 
-const domModule = (() => {
+let taskArray = [];
 
+const domMods = (() => {
+    function removeChildren(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
 
-    return 1;
+    const createElementAppend = (element, ID, parent) => {
+        let temp = document.createElement(element);
+        temp.id = ID;
+        document.getElementById(parent).appendChild(temp);
+        console.log(temp);
+    }
+
+    const ImgAppend = (ID, importedImage, parent) => {
+        let imgElement = document.createElement('img');
+        imgElement.id = ID;
+        imgElement.src = importedImage;
+        document.getElementById(parent).appendChild(imgElement);
+    }
+
+    const taskForm = () => {
+        const contentDiv = document.getElementById('content');
+
+        removeChildren(contentDiv);
+
+        //add labels for these inputs 6/28/22
+        createElementAppend('form', 'task-form', 'content');
+        createElementAppend('input', 'title', 'task-form');
+        createElementAppend('input', 'description', 'task-form');
+        createElementAppend('input', 'due-date', 'task-form');
+        createElementAppend('input', 'priority', 'task-form');
+        createElementAppend('input', 'notes', 'task-form');
+
+        const title = document.getElementById('title');
+        const description = document.getElementById('description');
+        const dueDate = document.getElementById('due-date');
+        const priority = document.getElementById('priority');
+        const notes = document.getElementById('notes');
+
+        title.textContent = 'test';
+        description.textContent = 'test';
+        dueDate.textContent = 'test';
+        priority.textContent = 'test';
+        notes.textContent = 'test';
+
+        //get this to display before doing anymore work
+
+    }
+
+    const taskList = () => {
+
+    }
+    return { createElementAppend, ImgAppend, taskForm };
 })();
 
-const createElementAppend = (element, ID, parent) => {
-    let temp = document.createElement(element);
-    temp.id = ID;
-    document.getElementById(parent).appendChild(temp);
-    console.log(temp);
-}
 
-const ImgAppend = (ID, importedImage, parent) => {
-    let imgElement = document.createElement('img');
-    imgElement.id = ID;
-    imgElement.src = importedImage;
-    document.getElementById(parent).appendChild(imgElement);
+
+class task {
+    constructor(title, description, targetDate, priority, notes) {
+        taskTitle = this.title;
+        taskDescription = this.description;
+        taskTargetDate = this.targetDate;
+        taskPriority = this.priority;
+        taskNotes = this.notes
+    }
+
+
+    //have the submitted task form send its inputs here to populate the new task object
+
 }
 
 const header = () => {
     document.getElementById('header').style.gridArea = 'hd';
 
-    createElementAppend('div', 'menu-icon', 'header');
-    createElementAppend('div', 'project-logo', 'header');
+    domMods.createElementAppend('div', 'menu-icon', 'header');
+    domMods.createElementAppend('div', 'project-logo', 'header');
 
     document.getElementById('project-logo').textContent = 'My ToDo App';
 
-    ImgAppend('test-img', home, 'menu-icon');
-
-    console.log('hello');
-
+    domMods.ImgAppend('test-img', home, 'menu-icon');
 }
 
 const sidebar = () => {
     document.getElementById('sidebar').style.gridArea = 'sb';
-    createElementAppend('div', 'home', 'sidebar');
-    createElementAppend('div', 'tasks', 'sidebar');
-    createElementAppend('div', 'project', 'sidebar');
+    domMods.createElementAppend('div', 'home', 'sidebar');
+    domMods.createElementAppend('div', 'tasks', 'sidebar');
+    domMods.createElementAppend('div', 'project', 'sidebar');
+
+    const sidebarHome = document.getElementById('home');
+    const sidebarTasks = document.getElementById('tasks');
+    const sidebarProject = document.getElementById('project');
+
+    sidebarTasks.onclick = function () { domMods.taskForm(); }
 
     document.getElementById('home').textContent = 'Home'
     document.getElementById('tasks').textContent = 'Tasks';
@@ -47,16 +104,19 @@ const sidebar = () => {
 }
 
 const content = () => {
-    createElementAppend('div', 'stuff', 'content');
+    domMods.createElementAppend('div', 'stuff', 'content');
     document.getElementById('stuff').textContent = 'content stuff';
+
+
+
 }
 
 const footer = () => {
-    createElementAppend('div', 'footer-stuff', 'footer')
+    domMods.createElementAppend('div', 'footer-stuff', 'footer')
     document.getElementById('footer-stuff').textContent = 'footer stuff'
 }
 header(), sidebar(), content(), footer();
 
-//TODO 6/27/22 create forms with neccessary inputs for creating
-// a new task(refer to Libre Writer Document) then/or build 
-// the class constructor required to handle the inputs
+//TODO 6/28/22 work on new task forms line 52 or something
+//the reason .textContent isnt working on the inputs is because it needs an assosiative label
+//which can then make use of .textContent
